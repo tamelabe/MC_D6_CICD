d8ed23eb491c5c73c3f7ce4c3b4a1c62754cac7b
Tamela Bears <tamelabe@student.21-school.ru>
2022-12-27 15:11:44 +0000

    test

--- src/cd_script.sh
+++ src/cd_script.sh
@@ -1,2 +1,3 @@
+ls
 scp ./cat/s21_cat tamelabe1@10.0.2.4:/home/tamelabe
-ssh tamelabe1@10.0.2.4 echo pass.txt | sudo -S mv s21_cat /usr/local/bin/
+ssh tamelabe1@10.0.2.4 cat pass.txt | sudo -S mv s21_cat /usr/local/bin/
